82799ea71dfca726c621f95bc4c479416646042a
Louis Alary <louis.alary@epsi.fr>
2024-04-07 11:20:28 +0200

    [ADD] : Room to clean endpoint

---
+++ src/Application/Rooms/Commands/AddRoomToClean/AddRoomToClean.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HotelManagement.Application.Common.Interfaces;
+using HotelManagement.Domain.Entities;
+using HotelManagement.Domain.Events;
+
+namespace HotelManagement.Application.Rooms.Commands.AddRoomToClean;
+
+public record AddRoomToClean : IRequest<int>
+{
+    public int ListId { get; init; }
+
+    public string? Title { get; init; }
+}
+
+public class CreateTodoItemCommandHandler : IRequestHandler<CreateTodoItemCommand, int>
+{
+    private readonly IApplicationDbContext _context;
+
+    public CreateTodoItemCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<int> Handle(CreateTodoItemCommand request, CancellationToken cancellationToken)
+    {
+        var entity = new TodoItem
+        {
+            ListId = request.ListId,
+            Title = request.Title,
+            Done = false
+        };
+
+        entity.AddDomainEvent(new TodoItemCreatedEvent(entity));
+
+        _context.TodoItems.Add(entity);
+
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return entity.Id;
+    }
+}
---
+++ src/Web/Endpoints/CleaningStaff.cs
@@ -0,0 +1,19 @@
+﻿using HotelManagement.Application.TodoLists.Queries.GetTodos;
+using HotelManagement.Application.WeatherForecasts.Queries.GetWeatherForecasts;
+
+namespace HotelManagement.Web.Endpoints;
+
+public class CleaningStaff
+{
+    public override void Map(WebApplication app)
+    {
+        app.MapGroup(this)
+            .RequireAuthorization()
+            .MapGet(GetRoomsToClean);
+    }
+
+    public Task<TodosVm> GetRoomsToClean(ISender sender)
+    {
+        return sender.Send(new GetTodosQuery());
+    }
+}
