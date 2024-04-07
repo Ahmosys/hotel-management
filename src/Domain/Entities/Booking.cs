148652e9ef1c789a87cf34b4dfb57b2b93e2b5d8
Ahmosys <ahmosyspro@protonmail.com>
2024-04-07 11:59:54 +0200

    feat: transform entity to fit DDD pattern and add new migration

--- src/Domain/Entities/Booking.cs
+++ src/Domain/Entities/Booking.cs
@@ -4,17 +4,50 @@ namespace HotelManagement.Domain.Entities;
 
 public class Booking : BaseAuditableEntity
 {
-    public DateTime StartDate { get; set; }
 
-    public DateTime EndDate { get; set; }
+    #region Properties
+
+    // The start date and end date of the booking
+    // can't be changed after the booking is created
+    public DateTimeOffset StartDate { get; private set; }
+
+    public DateTimeOffset EndDate { get; private set; }
 
     // By default a booking is not paid directly
-    public bool IsPaid { get; set; } = false;
+    public bool IsPaid { get; private set; } = false;
 
     // By default a booking is not cancelled
-    public bool IsCancelled { get; set; } = false;
+    public bool IsCancelled { get; private set; } = false;
 
-    public Room Room { get; set; } = null!;
+    public Room Room { get; private set; } = null!;
+
+    #endregion
+
+    #region Constructors
+
+    private Booking() { }
+
+    // Use a static factory method to create a new booking
+    public static Booking Create(DateTimeOffset startDate, DateTimeOffset endDate, Room room)
+    {
+        // Check if the start date is before the end date
+        if (startDate >= endDate)
+            throw new ArgumentException("Start date must be before end date.", nameof(startDate));
+
+        // Create a new booking
+        var booking = new Booking
+        {
+            StartDate = startDate,
+            EndDate = endDate,
+            Room = room
+        };
+
+        return booking;
+    }
+
+    #endregion
+
+    #region Methods
 
     // Checks if a booking is refundable
     // If the booking is within 48 hours of the start date, it is not refundable
@@ -22,4 +55,18 @@ public class Booking : BaseAuditableEntity
     {
         return StartDate.Subtract(DateTime.Now).TotalHours <= 48;
     }
+
+    // Pay the booking
+    public void MarkAsPay()
+    {
+        IsPaid = true;
+    }
+
+    // Cancel the booking
+    public void MarkAsCancelled()
+    {
+        IsCancelled = true;
+    }
+
+    #endregion
 }
--- src/Domain/Entities/Room.cs
+++ src/Domain/Entities/Room.cs
@@ -2,19 +2,7 @@ namespace HotelManagement.Domain.Entities;
 
 public class Room : BaseAuditableEntity
 {
-
-    public Room(int capacity, RoomStatus status, RoomType type, bool isClean, bool isAvailable)
-        : base()
-    {
-        Capacity = capacity;
-        Status = status;
-        Type = type;
-        IsClean = isClean;
-        IsAvailable = isAvailable;
-        // The rate of the room is based on the type of room
-        // so we never want to set it directly but rather calculate it
-        Rate = CalculateRate();
-    }
+    #region Properties
 
     public int Capacity { get; set; }
 
@@ -24,13 +12,46 @@ public class Room : BaseAuditableEntity
     
     public RoomType Type { get; set; }
 
-    public bool IsClean { get; set; }
+    public bool IsClean { get; private set; }
 
-    public bool IsAvailable { get; set; }
+    public bool IsAvailable { get; private set; }
 
     public IList<Booking> Bookings { get; private set; } = new List<Booking>();
 
-    public decimal CalculateRate()
+    #endregion
+
+    #region Constructors
+
+    private Room() { }
+
+    // Use a static factory method to create a new room
+    public static Room Create(int capacity, RoomStatus status, RoomType type, bool isClean, bool isAvailable)
+    {
+        // Check if the capacity is greater than zero
+        if (capacity <= 0)
+            throw new ArgumentException("Capacity must be greater than zero.", nameof(capacity));
+
+        // Create a new room
+        var room = new Room
+        {
+            Capacity = capacity,
+            Status = status,
+            Type = type,
+            IsClean = isClean,
+            IsAvailable = isAvailable
+        };
+
+        // Calculate the rate of the room based on the type
+        room.Rate = room.CalculateRate();
+
+        return room;
+    }
+
+    #endregion
+
+    #region Methods
+
+    private decimal CalculateRate()
     {
         // Here we use switch expression to define the rate of the room based on the type enum
         return Type switch
@@ -41,4 +62,36 @@ public class Room : BaseAuditableEntity
             _ => 0
         };
     }
+
+    public void MarkAsClean()
+    {
+        IsClean = true;
+    }
+
+    public void MarkAsDirty()
+    {
+        IsClean = false;
+    }
+
+    public void MarkAsAvailable()
+    {
+        IsAvailable = true;
+    }
+
+    public void MarkAsUnavailable()
+    {
+        IsAvailable = false;
+    }
+
+    public void AddBooking(Booking booking)
+    {
+        Bookings.Add(booking);
+    }
+
+    public void RemoveBooking(Booking booking)
+    {
+        Bookings.Remove(booking);
+    }
+
+    #endregion
 }
