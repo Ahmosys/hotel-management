89ec4b5d7d8d969a4ef39b6b017bf83ce422a975
Ahmosys <ahmosyspro@protonmail.com>
2024-04-09 20:44:36 +0200

    feat: add capability to cancel a room for a receptionist

--- src/Application/Bookings/Commands/CancelBooking/CancelBooking.cs
+++ src/Application/Bookings/Commands/CancelBooking/CancelBooking.cs
@@ -6,23 +6,27 @@ using HotelManagement.Domain.Repository;
 
 namespace HotelManagement.Application.Bookings.Commands.CancelBooking;
 
-[Authorize(Roles = Roles.Customer)]
+[Authorize(Roles = Roles.Customer + ", " + Roles.Receptionist)]
 public record CancelBookingCommand : IRequest
 {
     public int Id { get; init; }
+    public bool? WithRefund { get; init; }
 }
 
 public class CancelBookingCommandHandler : IRequestHandler<CancelBookingCommand>
 {
     private readonly IBookingRepository _bookingRepository;
+    private readonly IIdentityService _identityService;
     private readonly IUser _user;
 
     public CancelBookingCommandHandler(
         IBookingRepository bookingRepository,
+        IIdentityService identityService,
         IUser user
     )
     {
         _bookingRepository = bookingRepository;
+        _identityService = identityService;
         _user = user;
     }
 
@@ -32,11 +36,19 @@ public class CancelBookingCommandHandler : IRequestHandler<CancelBookingCommand>
 
         Guard.Against.NotFound(request.Id, booking);
 
-        // Only the user who created the booking can only cancel it
-        if (booking.CreatedBy != _user.Id)
-            throw new ForbiddenAccessException();
+        // If the user is a customer, they can only cancel their own bookings and cannot refund them after 48 hours
+        if (await _identityService.IsInRoleAsync(_user.Id!, Roles.Customer))
+        {
+            if (booking.CreatedBy != _user.Id)
+                throw new ForbiddenAccessException();
 
-        booking.Cancel();
+            booking.Cancel();
+        }
+        // If the user is a receptionist, they can cancel any booking and refund them at any time
+        else
+        {
+            booking.InstantCancel(request.WithRefund ?? false);
+        }
 
         await _bookingRepository.SaveChangesAsync(cancellationToken);
     }
